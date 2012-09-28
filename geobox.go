f4536cebab441eb9b1bb417023baceeb5bda0fab
Matthew Zimmerman <mzimmerman@gmail.com>
2012-09-28 10:15:43 -0400

    added ComputeSet functionality

--- geobox.go
+++ geobox.go
@@ -25,9 +25,11 @@ import (
 )
 
 type Location struct {
-	Lat     float64
-	Long    float64
-	Geocell string
+	Lat        float64
+	Long       float64
+	Slice      int
+	Resolution int
+	Geocell    string
 }
 
 func roundSliceDown(coord, slice float64) float64 {
@@ -38,11 +40,35 @@ func roundSliceDown(coord, slice float64) float64 {
 	return coord - remainder + slice
 }
 
+func (l Location) ComputeSet(tier int) (geocells []string) {
+	geocells = make([]string, (1+(2*tier))*(1+(2*tier)))
+	format := fmt.Sprintf("%%0.%df", l.Resolution)
+	fresolution := 1.0 / math.Pow10(l.Resolution)
+	fslice := float64(l.Slice) * fresolution
+	top := roundSliceDown(l.Lat, fslice)
+	right := roundSliceDown(l.Long, fslice)
+	left := right - fslice
+	bottom := top - fslice
+	walker := 0
+	for x := -tier; x <= tier; x++ {
+		lat_diff := fslice * float64(x)
+		for y := -tier; y <= tier; y++ {
+			long_diff := fslice * float64(y)
+			geocell := []string{fmt.Sprintf(format, top+lat_diff), fmt.Sprintf(format, left+long_diff), fmt.Sprintf(format, bottom+lat_diff), fmt.Sprintf(format, right+long_diff)}
+			geocells[walker] = strings.Join(geocell, "|")
+			walker++
+		}
+	}
+	return geocells
+}
+
 func Compute(lat, long float64, resolution, slice int) *Location {
 	format := fmt.Sprintf("%%0.%df", resolution)
 	location := new(Location)
 	location.Lat = lat
 	location.Long = long
+	location.Slice = slice
+	location.Resolution = resolution
 	fresolution := 1.0 / math.Pow10(resolution)
 	fslice := float64(slice) * fresolution
 	adj_lat := roundSliceDown(lat, fslice)
--- geobox_test.go
+++ geobox_test.go
@@ -19,6 +19,7 @@
 package geobox
 
 import (
+	"sort"
 	"strconv"
 	"testing"
 )
@@ -36,6 +37,7 @@ func TestLocations(t *testing.T) {
 		"37.78452", "-122.39532", 5, 2, "37.78452|-122.39532|37.78450|-122.39530",
 		"37.78452", "-122.39532", 5, 10, "37.78460|-122.39540|37.78450|-122.39530",
 		"37.78452", "-122.39532", 5, 25, "37.78475|-122.39550|37.78450|-122.39525",
+		"38.96367", "-76.501164", 1, 5, "39.0|-77.0|38.5|-76.5",
 	}
 	for row := 0; row < len(tests); row += 5 {
 		lat, _ := strconv.ParseFloat(tests[row].(string), 64)
@@ -48,6 +50,83 @@ func TestLocations(t *testing.T) {
 			t.Errorf("#%d - For %f, %f - %d - %d", row/5, lat, long, resolution, slice)
 			t.Errorf("Result - %s", location.Geocell)
 			t.Errorf("Answer - %s", geocode)
+		}
+	}
+}
+
+type ComputeTest struct {
+	Lat        float64
+	Long       float64
+	Slice      int
+	Resolution int
+	Geocells   []string
+	Tier       int
+}
+
+func TestComputeSet(t *testing.T) {
+	tests := []ComputeTest{
+		ComputeTest{
+			Lat:        37.78452,
+			Long:       -122.395320,
+			Slice:      5,
+			Resolution: 1,
+			Tier:       1,
+			Geocells: []string{
+				"37.5|-122.0|37.0|-121.5",
+				"38.0|-122.0|37.5|-121.5",
+				"38.5|-122.0|38.0|-121.5",
+				"37.5|-122.5|37.0|-122.0",
+				"38.0|-122.5|37.5|-122.0",
+				"38.5|-122.5|38.0|-122.0",
+				"37.5|-123.0|37.0|-122.5",
+				"38.0|-123.0|37.5|-122.5",
+				"38.5|-123.0|38.0|-122.5",
+			},
+		},
+		ComputeTest{
+			Lat:        37.78452,
+			Long:       -122.395320,
+			Slice:      5,
+			Resolution: 1,
+			Tier:       2,
+			Geocells: []string{
+				"37.0|-121.5|36.5|-121.0",
+				"37.5|-121.5|37.0|-121.0",
+				"38.0|-121.5|37.5|-121.0",
+				"38.5|-121.5|38.0|-121.0",
+				"39.0|-121.5|38.5|-121.0",
+				"37.0|-122.0|36.5|-121.5",
+				"37.5|-122.0|37.0|-121.5",
+				"38.0|-122.0|37.5|-121.5",
+				"38.5|-122.0|38.0|-121.5",
+				"39.0|-122.0|38.5|-121.5",
+				"37.0|-122.5|36.5|-122.0",
+				"37.5|-122.5|37.0|-122.0",
+				"38.0|-122.5|37.5|-122.0",
+				"38.5|-122.5|38.0|-122.0",
+				"39.0|-122.5|38.5|-122.0",
+				"37.0|-123.0|36.5|-122.5",
+				"37.5|-123.0|37.0|-122.5",
+				"38.0|-123.0|37.5|-122.5",
+				"38.5|-123.0|38.0|-122.5",
+				"39.0|-123.0|38.5|-122.5",
+				"37.0|-123.5|36.5|-123.0",
+				"37.5|-123.5|37.0|-123.0",
+				"38.0|-123.5|37.5|-123.0",
+				"38.5|-123.5|38.0|-123.0",
+				"39.0|-123.5|38.5|-123.0",
+			},
+		},
+	}
+	for x := range tests {
+		location := Compute(tests[x].Lat, tests[x].Long, tests[x].Resolution, tests[x].Slice)
+		geocells := location.ComputeSet(tests[x].Tier)
+		sort.Strings(geocells)
+		sort.Strings(tests[x].Geocells)
+		for y := range geocells {
+			if geocells[y] != tests[x].Geocells[y] {
+				t.Errorf("Failing as generated %s != test %s", geocells[y], tests[x].Geocells[y])
+			}
 		}
 	}
 }
